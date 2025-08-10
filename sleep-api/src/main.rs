c33a06ef2537b7dfef872b4b17f6c6bb1495d38f
ebigunso <kotaebi@gmail.com>
2025-08-11 01:24:31 +0900

    :adhesive_bandage: Environment agnostic migration file path

--- sleep-api/src/main.rs
+++ sleep-api/src/main.rs
@@ -13,10 +13,7 @@ use tokio::net::TcpListener;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
     let pool = connect().await?;
-    sqlx::migrate::Migrator::new(std::path::Path::new("/app/migrations"))
-        .await?
-        .run(&pool)
-        .await?;
+    sqlx::migrate!("../migrations").run(&pool).await?;
     let app = app::router(pool);
     let listener = TcpListener::bind("0.0.0.0:8080").await?;
     axum::serve(listener, app).await?;
